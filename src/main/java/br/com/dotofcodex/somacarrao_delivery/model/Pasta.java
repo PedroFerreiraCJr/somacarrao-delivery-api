e7cab2d4d4f0786c3c4b47e4ac819721d2cbbcfe
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-04-25 04:29:58 -0300

    remove get/set serializable

--- src/main/java/br/com/dotofcodex/somacarrao_delivery/model/Pasta.java
+++ src/main/java/br/com/dotofcodex/somacarrao_delivery/model/Pasta.java
@@ -58,14 +58,6 @@ public class Pasta implements Serializable {
 		return available;
 	}
 
-	public void setAvailable(Boolean available) {
-		this.available = available;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
